966dfc32b219bc2b8e87dbf79ae9e903004aed2f
ElegantFalcon <109782243+ElegantFalcon@users.noreply.github.com>
2022-12-28 23:29:16 +0530

    V3

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,18 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log('Hello Fam')
+    }
+
+    const handleClickAgain = (name) => {
+        console.log('hello' + name)
+    }
+
     return ( 
         <div className = "home">
             <h2>Homepage</h2>
-
+             <button  onClick={handleClick}>Click me</button>
+             <button  onClick={() => handleClickAgain('Mario')}>Click me</button>
         </div>
      );
 }
