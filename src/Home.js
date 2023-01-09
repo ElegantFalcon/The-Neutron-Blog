f316a0e2cb2f26fb6dd0ae3848a2df0d11d3ebca
nikhil <109782243+ElegantFalcon@users.noreply.github.com>
2023-01-09 22:33:22 +0530

    V7.4

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,9 @@
-import {useState} from 'react' ; 
+import {useState, useEffect} from 'react' ; 
 import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs,setBlogs] = useState([
-        { title :  'My new Website' , body : 'lorem ipsum dms m...', author :'mario', id : 1},
+        { title :  'My new Website' , body : 'lorem ipsum ...', author :'mario', id : 1},
         { title :  'Welcome party' , body : 'lorem ipsum...', author :'yoshi', id : 2},
         { title :  'Web dev top tips' , body : 'lorem ipsum...', author :'mario', id : 3},
     ]);
@@ -12,6 +12,11 @@ const Home = () => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
+
+    useEffect(()  => {
+        console.log
+        
+    }, []);
 
     return ( 
         <div className = "home">
